5fef795b5dd21d75a2f26eff130916eb84e7355a
Jailson Prado <jailsondpd@gmail.com>
2022-02-20 17:52:25 -0300

    first commit in project React Native

---
+++ App.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import {} from 'react-native'
+
+// API KEY == https://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=ultra&apiKey=3a77fbcc862738513ec6
